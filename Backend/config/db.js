358802b1173f14f9c45db6f9a90ca0d9e185ea20
Jin1won <wlsdnjs16521@gmail.com>
2022-03-15 23:47:44 +0900

    3달마다 실행되는 batch로직 작성

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,15 @@
 /* eslint-disable import/extensions */
 import mongoose from "mongoose";
 import keys from "./keys.js";
+import schedule from "node-schedule";
 
 export default async () => {
   try {
     await mongoose.connect(keys.mongoURI);
+    //batch jobs
+    schedule.scheduleJob("* * 1 1,4,7,9 *", async () => {
+      console.log("Reset totalScore in commit db");
+    });
   } catch (err) {
     console.error(err.message);
   }
